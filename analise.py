c9fbbf2986056c14c70c182a789cdd002c2b90ac
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-08-11 11:51:29 -0300

    Tratamento de dados, etapa 4

--- analise.py
+++ analise.py
@@ -3,3 +3,10 @@ from dataset import dados1
 # print(dados1.head(10))
 print(dados1['IDADE'].value_counts())
 print(dados1['IDADE'].value_counts().sort_index())
+
+menores_12 = dados1.query('IDADE <= 12')[
+    'NO_MUNICIPIO_RESIDENCIA'].value_counts().sort_index()
+
+maiores_11 = dados1.loc[dados1.IDADE > 11]
+
+print(maiores_11.head(40))
